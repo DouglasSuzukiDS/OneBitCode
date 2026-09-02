c2ec514f1db076812bc52a073fd93ad1d0a6c017
Nick <kaido19@outlook.com>
2026-09-01 21:01:27 -0300

    ex03 30DesafiosJS

--- 30DesafiosJS/ex03/ex03.js
+++ 30DesafiosJS/ex03/ex03.js
@@ -1,47 +1,42 @@
 /*
 Desafio 03: Linguagem Limitada 🚫 (Arrays, Estruturas Básicas)**
 
-Um dispositivo portátil encontrado no planeta Anelius está sendo estudado pela equipe de pesquisadores do Instituto de Tecnologia da Federação. Ele funciona a base de uma linguagem desconhecida e limitada. Para estender suas funcionalidades, foi solicitado que você crie um procedimento que seja capaz de inverter listas de forma não destrutiva, ou seja, sem alterar a lista original, porém utilizando apenas recursos básicos de repetição (for, while, etc)**.
-Escreva uma função que recebe um array e retorne um novo array com todas as posições invertidas do original sem alterá-lo. Não utilize os métodos do objeto global Array do javascript (reverse, map, forEach, etc)**.
+   Um dispositivo portátil encontrado no planeta Anelius está sendo estudado pela equipe de pesquisadores do Instituto de Tecnologia da Federação. Ele funciona a base de uma linguagem desconhecida e limitada. Para estender suas funcionalidades, foi solicitado que você crie um procedimento que seja capaz de inverter listas de forma não destrutiva, ou seja, sem alterar a lista original, porém utilizando apenas recursos básicos de repetição (for, while, etc)**.
+   Escreva uma função que recebe um array e retorne um novo array com todas as posições invertidas do original sem alterá-lo. Não utilize os métodos do objeto global Array do javascript (reverse, map, forEach, etc)**.
 
-***ENTRADA:***
-
-* [0, 9, 6, 8, 9, 1, 5, 7]
-
-
-* ['Oh', 'Hi', 'Mark']
-
-
-* [false, true, true, true]
-
-
-* ["It's", "not", true, 0]
+   ***ENTRADA:***
+      * [0, 9, 6, 8, 9, 1, 5, 7]
+      * ['Oh', 'Hi', 'Mark']
+      * [false, true, true, true]
+      * ["It's", "not", true, 0]
 
 
 
-***SAÍDA:***
-
-* [7, 5, 1, 9, 8, 6, 9, 0]
-
-
-* ['Mark', 'Hi', 'Oh']
-
-
-* [true, true, true, false]
-
-
-* [0, true, 'not', "It's"]
-
-
-
----
-
-**
+   ***SAÍDA:***
+         * [7, 5, 1, 9, 8, 6, 9, 0]
+         * ['Mark', 'Hi', 'Oh']
+         * [true, true, true, false]
+         * [0, true, 'not', "It's"]
 */
 
+function reverseArray(arr) {
+   let reverseArr = []
+   let len = arr.length
 
-// ========================================
-// SOLUÇÃO
-// ========================================
+   while (reverseArr.length < arr.length) {
+      reverseArr.push(arr[len - 1])
+      len--
+   }
 
+   return reverseArr
+}
 
+array1 = [0, 9, 6, 8, 9, 1, 5, 7]
+array2 = ['Oh', 'Hi', 'Mark']
+array3 = [false, true, true, true]
+array4 = ["It's", "not", true, 0]
+
+console.log(reverseArray(array1))
+console.log(reverseArray(array2))
+console.log(reverseArray(array3))
+console.log(reverseArray(array4))
